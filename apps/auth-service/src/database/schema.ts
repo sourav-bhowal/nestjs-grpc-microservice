2634a0eff91c8e9a1565c5c129f8a52bf3d91810
Sourav Bhowal <souravbhowal15@gmail.com>
2026-06-21 21:16:07 +0530

    db

---
+++ apps/auth-service/src/database/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+  updated_at: timestamp('updated_at').notNull().defaultNow(),
+});
